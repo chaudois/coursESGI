916c28b8409c70a1e086997df4c5e1198eed57c6
chaudois <damien.chaudois@gmail.com>
2017-01-12 21:44:50 +0100

    deplacement de la demo Maven de cours java vers cours outils dev et suppression du dossier .git dans le dossier cours java

---
+++ Cours Outils Developpeur/demoMaven/src/test/java/com/esgi/AppTest.java
@@ -0,0 +1,18 @@
+package com.esgi;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    @Test
+    public void should_assert_true(){
+        assertTrue(true);
+    }
+}
